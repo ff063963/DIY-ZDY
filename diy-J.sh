d5abac9ad1f5afb0b919a7716189e73f99ace2f0
zxyadc <101701125+zxyadc@users.noreply.github.com>
2022-10-27 20:16:39 +0800

    Update diy-J.sh

--- diy-J.sh
+++ diy-J.sh
@@ -29,7 +29,7 @@ echo "RELEASE_KEY_PASSWORD=TVBoxOSC" >>$CURRENT_DIR/$DIR/gradle.properties
 cp $CURRENT_DIR/DIY/epg_data.json $CURRENT_DIR/$DIR/app/src/main/assets/epg_data.json
 #版本号
 sed -i '/android:versionName="1.1.0"/a' $CURRENT_DIR/$DIR/app/src/main/AndroidManifest.xml
-sed -i 's/关于/关于                                                     1.1.0/g'  $CURRENT_DIR/$DIR/app/src/main/res/layout/fragment_model.xml
+sed -i 's/关于/关于                                               1.1.0/g'  $CURRENT_DIR/$DIR/app/src/main/res/layout/fragment_model.xml
 #cp $CURRENT_DIR/DIY/AndroidManifest.xml $CURRENT_DIR/$DIR/app/src/main/AndroidManifest.xml
 #名称修改
 sed -i 's/TVBox/TVBoxPy/g' $CURRENT_DIR/$DIR/app/src/main/res/values/strings.xml
