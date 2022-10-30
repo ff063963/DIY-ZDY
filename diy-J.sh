731131b1321aac9ef18a72a749908d5179fd21a7
zxyadc <101701125+zxyadc@users.noreply.github.com>
2022-10-31 01:33:28 +0800

    Update diy-J.sh

--- diy-J.sh
+++ diy-J.sh
@@ -46,6 +46,8 @@ mv $CURRENT_DIR/DIY/app_icon4.png $CURRENT_DIR/$DIR/app/src/main/res/drawable-xx
 #播放界面修改
 
 sed -i 's/"片头片尾"/ ""/g' file $CURRENT_DIR/$DIR/app/src/main/res/layout/player_vod_control_view.xml
+sed -i 's/"01:00"/ "片头01:00"/g' file $CURRENT_DIR/$DIR/app/src/main/res/layout/player_vod_control_view.xml
+sed -i 's/"01:00"/ "片尾01:00"/2g' file $CURRENT_DIR/$DIR/app/src/main/res/layout/player_vod_control_view.xml
 #cp $CURRENT_DIR/DIY/player_vod_control_view.xml $CURRENT_DIR/$DIR/app/src/main/res/layout/player_vod_control_view.xml
 
 #默认设置修改
