34671e7db635b3c042af449cdffc0fb75d4184b7
zxyadc <101701125+zxyadc@users.noreply.github.com>
2022-10-31 17:31:02 +0800

    Update diy-J.sh

--- diy-J.sh
+++ diy-J.sh
@@ -48,7 +48,7 @@ sed -i 's/"刷新"//g' $CURRENT_DIR/$DIR/app/src/main/res/layout/player_vod_cont
 sed -i 's/"上一集"/ "上集"/g' $CURRENT_DIR/$DIR/app/src/main/res/layout/player_vod_control_view.xml
 sed -i 's/"下一集"/ "下集"/g' $CURRENT_DIR/$DIR/app/src/main/res/layout/player_vod_control_view.xml
 sed -i 's/片头片尾//' $CURRENT_DIR/$DIR/app/src/main/res/layout/player_vod_control_view.xml
-sed -i 's/"00:00"/ "10:00"/'  $CURRENT_DIR/$DIR/app/src/main/res/layout/player_vod_control_view.xml
+$sed -i 's/"00:00"/ "10:00"/'  $CURRENT_DIR/$DIR/app/src/main/res/layout/player_vod_control_view.xml
 #sed -i 's/"00:00"/ "20:00"/2g' $CURRENT_DIR/$DIR/app/src/main/res/layout/player_vod_control_view.xml
 #cp $CURRENT_DIR/DIY/player_vod_control_view.xml $CURRENT_DIR/$DIR/app/src/main/res/layout/player_vod_control_view.xm
 
