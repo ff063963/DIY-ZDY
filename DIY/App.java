6afc9fbc864993e3f2d2f3105f0e0b14abf97b21
zxyadc <101701125+zxyadc@users.noreply.github.com>
2022-11-01 00:33:39 +0800

    默认设置

--- DIY/App.java
+++ DIY/App.java
@@ -57,9 +57,13 @@ public class App extends MultiDexApplication {
         Hawk.init(this).build();
         Hawk.put(HawkConfig.DEBUG_OPEN, false);
         if (!Hawk.contains(HawkConfig.PLAY_TYPE)) {
-            Hawk.put(HawkConfig.PLAY_TYPE, 1);
-           Hawk.put(HawkConfig.HOME_REC, 1);       // Home Rec 0=豆瓣, 1=推荐, 2=历史
-           Hawk.put(HawkConfig.IJK_CODEC, "硬解码");// IJK Render 软解码, 硬解码
+           
+        Hawk.put(HawkConfig.HOME_REC, 2);       // Home Rec 0=豆瓣, 1=推荐, 2=历史
+        Hawk.put(HawkConfig.PLAY_TYPE, 1);      // Player   0=系统, 1=IJK, 2=Exo
+        Hawk.put(HawkConfig.IJK_CODEC, "硬解码");// IJK Render 软解码, 硬解码
+//        putDefault(HawkConfig.HOME_NUM, 2);       // History Number
+//        putDefault(HawkConfig.DOH_URL, 2);        // DNS
+//        putDefault(HawkConfig.SEARCH_VIEW, 1);    // Text or Picture  
         }
     }
 
