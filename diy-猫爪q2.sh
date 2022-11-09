1c4877de5c647b2216aefa7a422e62772af2d969
zxyadc <101701125+zxyadc@users.noreply.github.com>
2022-11-09 23:52:33 +0800

    Update diy-猫爪q2.sh

--- diy-猫爪q2.sh
+++ diy-猫爪q2.sh
@@ -27,11 +27,11 @@ sed -i 's/1.0.0/1.1.9/g' $CURRENT_DIR/$DIR/app/build.gradle
 #cp $CURRENT_DIR/DIY/fragment_model.xml $CURRENT_DIR/$DIR/app/src/main/res/layout/fragment_model.xml 
 
 sed -i 's/epgApi"/epgApi" android:visibility="gone"/g' $CURRENT_DIR/$DIR/app/src/main/res/layout/fragment_model.xml
-sed -i 's/关于/关于                                                                   1.1.9/g' $CURRENT_DIR/$DIR/app/src/main/res/layout/fragment_model.xml
+sed -i 's/关于/关于\\n                                                                   1.1.9/g' $CURRENT_DIR/$DIR/app/src/main/res/layout/fragment_model.xml
 #sed -i 's/android:text=">"/android:text="$1.1.9>"/g' $CURRENT_DIR/$DIR/app/src/main/res/layout/fragment_model.xml
 #sed –i 's/>"/1.1.9>"/g' $CURRENT_DIR/$DIR/app/src/main/res/layout/fragment_model.xml
-#sed –i 's/android:text=">"/android:text="1.1.9  >"/5g'  $CURRENT_DIR/$DIR/app/src/main/res/layout/fragment_model.xml
-sed '/关于/,/white/s/android:text=">"/android:text="1.1.9 >"/'   $CURRENT_DIR/$DIR/app/src/main/res/layout/fragment_model.xml
+sed –i 's/android:text=">"/android:text="1.1.9  >"/1'  $CURRENT_DIR/$DIR/app/src/main/res/layout/fragment_model.xml
+#sed '/关于/,/white/s/android:text=">"/android:text="1.1.9 >"/'   $CURRENT_DIR/$DIR/app/src/main/res/layout/fragment_model.xml
 
 cp $CURRENT_DIR/DIY/dialog_about_关于.xml $CURRENT_DIR/$DIR/app/src/main/res/layout/dialog_about.xml 
 
