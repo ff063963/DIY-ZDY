444cb6843d0222e0027a3e2c84d3329f032001a5
zxyadc <101701125+zxyadc@users.noreply.github.com>
2022-11-09 22:30:02 +0800

    Update diy-猫爪q2.sh

--- diy-猫爪q2.sh
+++ diy-猫爪q2.sh
@@ -24,6 +24,8 @@ echo "RELEASE_KEY_PASSWORD=TVBoxOSC" >>$CURRENT_DIR/$DIR/gradle.properties
 sed -i 's/1.0.0/1.1.9/g' $CURRENT_DIR/$DIR/app/build.gradle
 
 #设置界面
+cp $CURRENT_DIR/DIY/fragment_model.xml $CURRENT_DIR/$DIR/app/src/main/res/layout/fragment_model.xml 
+
 sed -i 's/epgApi"/epgApi" android:visibility="gone"/g' $CURRENT_DIR/$DIR/app/src/main/res/layout/fragment_model.xml
 sed -i 's/关于/关于                                                                   1.1.9/g' $CURRENT_DIR/$DIR/app/src/main/res/layout/fragment_model.xml
 sed -i 's/android:text=">"/android:text="1.1.9>"/g' $CURRENT_DIR/$DIR/app/src/main/res/layout/fragment_model.xml
