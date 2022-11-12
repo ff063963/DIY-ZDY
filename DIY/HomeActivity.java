661597c7880eb981244f0b1f9d743335aa7fd8e2
zxyadc <101701125+zxyadc@users.noreply.github.com>
2022-11-13 02:53:50 +0800

    Update HomeActivity.java

--- DIY/HomeActivity.java
+++ DIY/HomeActivity.java
@@ -258,7 +258,7 @@ public class HomeActivity extends BaseActivity {
      if (HomeShow) {
         if (home != null && home.getName() != null && !home.getName().isEmpty())
             tvName.setText(home.getName());
-     } }
+     } 
         if (dataInitOk && jarInitOk) {
             showLoading();
             sourceViewModel.getSort(ApiConfig.get().getHomeSourceBean().getKey());
