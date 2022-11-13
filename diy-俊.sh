40297fa2f1af335812f999e2c408ef92f4a14e7e
zxyadc <101701125+zxyadc@users.noreply.github.com>
2022-11-13 22:42:32 +0800

    Update diy-俊.sh

--- diy-俊.sh
+++ diy-俊.sh
@@ -49,7 +49,8 @@ cp $CURRENT_DIR/DIY/fragment_model.xml $CURRENT_DIR/$DIR/app/src/main/res/layout
 sed -i 's/1.0.0 /1.2.0/g' $CURRENT_DIR/$DIR/app/src/main/res/layout/fragment_model.xml
 
 //删除播放器
- sed '/播放器/d'$CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/util/PlayerHelper.java
+cp $CURRENT_DIR/DIY/PlayerHelper.java $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/util/PlayerHelper.java
+ #sed '/播放器/d'$CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/util/PlayerHelper.java
 #sed -i 's/播放器 //g' $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/util/PlayerHelper.java
 
 //按键背景颜色
