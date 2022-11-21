2d15e64f0f1fa271f7657a359c834102a8c36907
zxyadc <101701125+zxyadc@users.noreply.github.com>
2022-11-21 20:38:32 +0800

    Update diy-俊.sh

--- diy-俊.sh
+++ diy-俊.sh
@@ -96,9 +96,6 @@ sed -i 's/6000/5000/g'  $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc
 # 默认设置
 cp $CURRENT_DIR/DIY/App.java $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/base/App.java  
 
- 
-
-
 #自定义epg
 cp $CURRENT_DIR/DIY/epg_data.json $CURRENT_DIR/$DIR/app/src/main/assets/epg_data.json
 
@@ -106,7 +103,7 @@ cp $CURRENT_DIR/DIY/epg_data.json $CURRENT_DIR/$DIR/app/src/main/assets/epg_data
 cp $CURRENT_DIR/DIY/colors自定义.xml $CURRENT_DIR/$DIR/app/src/main/res/values/colors.xml
 
 #名称修改
-sed -i 's/TVBox/TVBoxPy/g' $CURRENT_DIR/$DIR/app/src/main/res/values/strings.xml
+sed -i 's/TVBox/TVBoxPQ/g' $CURRENT_DIR/$DIR/app/src/main/res/values/strings.xml
 
 #图标修改
 cp $CURRENT_DIR/DIY/原版透明.png $CURRENT_DIR/$DIR/app/src/main/res/drawable-hdpi/app_icon.png
