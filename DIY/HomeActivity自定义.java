aad1e00f91716f0485c219b63cd95a4670e7ad05
zxyadc <101701125+zxyadc@users.noreply.github.com>
2022-11-28 14:59:24 +0800

    Update HomeActivity自定义.java

--- DIY/HomeActivity自定义.java
+++ DIY/HomeActivity自定义.java
@@ -616,8 +616,8 @@ public class HomeActivity extends BaseActivity {
             SelectDialog<SourceBean> dialog = new SelectDialog<>(HomeActivity.this);
             TvRecyclerView tvRecyclerView = dialog.findViewById(R.id.list);
             int spanCount;
-            spanCount = (int)Math.floor(sites.size()/1);
-            spanCount = Math.min(spanCount, 10);
+            spanCount = (int)Math.floor(sites.size()/10);
+            spanCount = Math.min(spanCount, 2);
             tvRecyclerView.setLayoutManager(new V7GridLayoutManager(dialog.getContext(), spanCount+1));
             ConstraintLayout cl_root = dialog.findViewById(R.id.cl_root);
             ViewGroup.LayoutParams clp = cl_root.getLayoutParams();
