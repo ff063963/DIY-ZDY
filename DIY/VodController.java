c18b1825d8f13a61bec6a795679b4a657304012d
zxyadc <101701125+zxyadc@users.noreply.github.com>
2022-11-28 18:33:07 +0800

    Update VodController.java

--- DIY/VodController.java
+++ DIY/VodController.java
@@ -226,6 +226,7 @@ public class VodController extends BaseController {
         mBottomRoot = findViewById(R.id.bottom_container);
         mTopRoot1 = findViewById(R.id.tv_top_l_container);
         mTopRoot2 = findViewById(R.id.tv_top_r_container);
+        mTopRoot3 = findViewById(R.id.screen_display);
         mParseRoot = findViewById(R.id.parse_root);
         mGridView = findViewById(R.id.mGridView);
         mPlayerRetry = findViewById(R.id.play_retry);
